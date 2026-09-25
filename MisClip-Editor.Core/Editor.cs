5d419cec8822e1f599078668e208ba7d459147f4
KavunSvizhyi <maksrudyka@gmail.com>
2026-09-25 16:43:15 +0300

    Added Not Implemented methods to Editor

--- MisClip-Editor.Core/Editor.cs
+++ MisClip-Editor.Core/Editor.cs
@@ -1,11 +1,51 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using System.Threading.Tasks.Dataflow;
+using FFMpegCore;
 
 namespace MisClip_Editor.Core
 {
     public static class Editor
     {
-        
+        public static void GetSnapshot()
+        {
+            throw new NotImplementedException();
+        }
+
+        public static void GetGif()
+        {
+            throw new NotImplementedException();
+        }
+
+        public static void JoinVideo()
+        {
+            throw new NotImplementedException();
+        }
+
+        public static void JoinImage()
+        {
+            throw new NotImplementedException();
+        }
+
+        public static void Cut()
+        {
+            throw new NotImplementedException();
+        }
+
+        public static void Mute()
+        {
+            throw new NotImplementedException();
+        }
+
+        public static void ExtractAudio()
+        {
+            throw new NotImplementedException();
+        }
+
+        public static void AddOrReplaceAudio()
+        {
+            throw new NotImplementedException();
+        }
     }
 }
